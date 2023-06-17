d76b8b83b4b3727262822ba408b6aeeec3fa2148
Mike Ellis <michaelellis87@gmail.com>
2023-06-17 02:41:11 -0300

    fix/cors

--- src/routes/product.js
+++ src/routes/product.js
@@ -7,7 +7,12 @@ const productController = new ProductController();
 const constants = require('../constants')
 
 Router.use(express.json());
-
+Router.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-api-key');
+    res.setHeader('Content-Type', 'application/json');
+    next(); 
+});
 Router.post('/products', verifyToken, verifyPermission(), (req, res, next) => productController.createProduct(req, res, next));
 Router.get('/products', verifyToken, verifyPermission(constants.roles.all), (req, res, next) => productController.getProducts(req, res, next));
 Router.get('/products/:id', verifyToken, verifyPermission(constants.roles.all), (req, res, next) => productController.getProduct(req, res, next));
--- src/routes/sale.js
+++ src/routes/sale.js
@@ -7,7 +7,12 @@ const salesController = new SalesController();
 const constants = require('../constants')
 
 Router.use(express.json());
-
+Router.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-api-key');
+    res.setHeader('Content-Type', 'application/json');
+    next(); 
+});
 Router.post('/sales', verifyToken, verifyPermission([constants.roles.employee]), (req, res, next) => salesController.createSale(req, res, next));
 Router.get('/sales', verifyToken, verifyPermission([constants.roles.employee]), (req, res, next) => salesController.getSales(req, res, next));
 Router.get('/sales/:id', verifyToken, verifyPermission([constants.roles.employee]), (req, res, next) => salesController.getSale(req, res, next));
